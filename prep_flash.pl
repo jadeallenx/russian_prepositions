7d64a6f3973726bc4ecc4d6410c701b07aeac267
Mark Allen <mrallen1@yahoo.com>
2018-07-25 23:45:58 -0500

    Last minute fixes and tweaks

--- prep_flash.pl
+++ prep_flash.pl
@@ -4,17 +4,19 @@ use 5.014;
 use utf8;
 
 use Text::CSV qw(csv);
-use Data::Printer;
+use List::Util qw(shuffle);
 
 binmode STDOUT, ":utf8";
 
 # We are going to parse our input csv file and then generate our decks
 
+my $hicolor = "Blue";
+
 sub highlight {
     my ($word, $o) = @_;
-    my $r = $o =~ s|\b$word\b|<span style="color:SkyBlue">$word</span>|r;
+    my $r = $o =~ s|\b$word\b|<span style="color:$hicolor">$word</span>|r;
     my $tword = ucfirst($word);
-    my $s = $r =~ s|^$tword\b|<span style="color:SkyBlue">$tword</span>|r;
+    my $s = $r =~ s|^$tword\b|<span style="color:$hicolor">$tword</span>|r;
     return $s;
 }
 
@@ -33,7 +35,15 @@ my $h = csv( in => "russian_prepositions.csv",
 my @hf;
 my @out;
 
-for my $hr ( @{ $h } ) {
+# We are shuffling the list because we want the deck order to be
+# randomized, so that not all of the same prepositions show up
+# on the same day and time.
+#
+# Studies have shown that too much repetition of similar things
+# leads them to be jumbled together.
+#
+# See http://www.victoria.ac.nz/lals/about/staff/publications/paul-nation/foreign-language_1125.pdf
+for my $hr ( shuffle @{ $h } ) {
     my $front = highlight($hr->{word}, $hr->{ru_sentence});
     my $back = $hr->{word} . " : " . $hr->{english} . " : " . get_case($hr->{case}) . "<br>" . $hr->{en_sentence};
     my $slide = [$front, $back];
